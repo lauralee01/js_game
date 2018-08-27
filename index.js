bba688c7aaf87f7738b9ae7512b4d86f2bfe9e47
sajicode <sajioloye@gmail.com>
2018-08-27 17:46:53 +0100

    moving the ball

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(20, 40, 80, 80);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 50, 0, Math.PI*2, false);
-ctx.fillStyle = "yellow";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(100, 10, 200, 70);
-ctx.strokeStyle = "rgba(200, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	x += dx;
+	y += dy; 
+}
+setInterval(draw, 10);
