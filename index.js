64d8cb2cefe5d6c6a1cdccbabe846becbf9d1ac8
sajicode <sajioloye@gmail.com>
2018-09-01 16:13:48 +0100

    Controlling the ball collision

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
+
 
 function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -16,7 +18,20 @@ function drawBall() {
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+	 
+
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+		ctx.fillStyle = "#fff";
+		dx = -dx;
+		
+	}
+	if(x + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+		ctx.fillStyle = "#000";
+		dy = -dy;
+		
+	}
+
 	x += dx;
-	y += dy; 
+	y += dy;
 }
 setInterval(draw, 10);
