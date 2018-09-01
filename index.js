6b9aa3c358c410bd052ee8062dd764c65d4fc60c
sajicode <sajioloye@gmail.com>
2018-09-01 16:42:17 +0100

    Paddle and keyboard controls

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var paddleY = canvas.height-paddleHeight;
+var rightPressed = false;
+var leftPressed = false;
 
-
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+	ctx.fillStyle = "red";
+	ctx.fill();
+	ctx.closePath();
+}
 function drawBall() {
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-	ctx.fillStyle = "#0095DD";
+	ctx.fillStyle = "red";
 	ctx.fill();
 	ctx.closePath();
 }
@@ -18,20 +30,46 @@ function drawBall() {
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
-	 
+	drawPaddle();
 
 	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-		ctx.fillStyle = "#fff";
 		dx = -dx;
 		
 	}
 	if(x + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-		ctx.fillStyle = "#000";
 		dy = -dy;
-		
+
+	}
+
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+		paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0) {
+		paddleX -= 7;
 	}
 
 	x += dx;
 	y += dy;
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
 }
 setInterval(draw, 10);
